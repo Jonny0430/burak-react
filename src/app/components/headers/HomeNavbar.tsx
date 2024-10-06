24547ead2a9cdff9c51cdd90511067a96fe2be64
Jonny0430 <ahadjon697gmail.com>
2024-10-06 21:59:15 +0900

    feat: Fixed HomeNavbar publishing process and public

---
+++ src/app/components/headers/HomeNavbar.tsx
@@ -0,0 +1,14 @@
+import { Container, Stack } from "@mui/material";
+import React from "react";
+
+export function HomeNavbar() {
+    return (
+         <div className="home-navbar">
+            <Container sx={{ mt: "55px", height: "642px" }}>
+                <Stack>NAVBAR</Stack>
+                <Stack>DETAIL</Stack>
+            </Container>
+         </div>
+    );
+
+}
